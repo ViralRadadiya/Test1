cc019e4f41b0ee829aa087325960f2b994813b5e
Viral Radadiya <vrlradadiya@gmail.com>
2022-02-22 14:32:09 +0530

    init

---
+++ Demo/src/main/java/com/example/demo/Adwings.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Adwings {
+
+    public static void showToast(Context context){
+        Toast.makeText(context, "jhd", Toast.LENGTH_SHORT).show();
+    }
+}
